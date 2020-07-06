fe3c0d110f0a5352fcb3c4aaa11bbe8c5cbbe458
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-06 12:45:40 -0400

    Add initial variables and event listeners

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+//* Variables ###############
 let board = document.querySelector(".board");
+let resetBtn = document.querySelector(".reset-btn");
+
+//* Event listeners ###############
+board.addEventListener("click", (e) => {
+	console.log(e.target);
+});
+
+resetBtn.addEventListener("click", (e) => {
+	console.log(e.target);
+});

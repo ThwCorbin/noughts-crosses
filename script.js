fd94c1f4cd219a82095fe29f583bf35f65ce3c2a
Tom Corbin <30200976+ThwCorbin@users.noreply.github.com>
2020-07-06 13:44:05 -0400

    Move function out of event listener, add checkSquare() to handle marking squares, add changePlayer() to switch players

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 //* Variables ###############
 let board = document.querySelector(".board");
 let resetBtn = document.querySelector(".reset-btn");
+let messageText = document.querySelector(".message-text");
+let currentColor = "red";
+let currentPlayer = "blue";
+
+//* Functions ###############
+
+let changePlayer = () => {
+	currentColor = currentColor === "red" ? "blue" : "red";
+	currentPlayer = currentPlayer === "Noughts" ? "Crosses" : "Noughts";
+	messageText.style.color = currentColor;
+	messageText.textContent = currentPlayer;
+};
+
+let checkSquare = (e) => {
+	if (e.target.className.includes("picked")) {
+		messageText.textContent = `This square is taken, ${currentColor}!`;
+		return;
+	}
+	if (e.target.classList[0] === "o-x-text") {
+		console.log(e.target, "target is p");
+		e.target.classList.add("picked");
+		e.target.parentElement.classList.add(`${currentColor}`, "picked");
+		changePlayer();
+		return;
+	}
+	if (e.target.classList[0] === "square") {
+		console.log(e.target, "target is div square");
+		e.target.classList.add(`${currentColor}`, "picked");
+		e.target.firstElementChild.classList.add("picked");
+		changePlayer();
+		return;
+	}
+};
 
 //* Event listeners ###############
-board.addEventListener("click", (e) => {
-	console.log(e.target);
-});
+board.addEventListener("click", checkSquare);
 
 resetBtn.addEventListener("click", (e) => {
 	console.log(e.target);
